5807339807d665c05e865067c4ed351bbe414f4a
Utkcool <utkarsh.shakya@gmail.com>
2024-09-30 17:28:57 +0530

    windows user

--- src/test/java/testCases/TC002_Search.java
+++ src/test/java/testCases/TC002_Search.java
@@ -9,4 +9,13 @@ public class TC002_Search {
 	{
 		System.out.println("this is search functionality");
 	}
+	
+	@Test
+	public void searchTwo()
+	{
+		System.out.println("this is search functionality based on search ");
+		System.out.println("this is search functionality 2");
+		System.out.println("this is search functionality 3");
+		System.out.println("this is search functionality 4");
+	}
 }
---
+++ src/test/java/testCases/TC_003_DropdownTest.java
@@ -0,0 +1,20 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class TC_003_DropdownTest {
+	@Test
+	public void dropDown()
+	{
+		System.out.println("this is dropdowntest");
+	}
+	
+	@Test
+	public void dropdownTwo()
+	{
+		System.out.println("this is dropdowntest based on search ");
+		System.out.println("this is dropdowntest 2");
+		System.out.println("this is dropdowntest 3");
+		System.out.println("this is dropdowntest 4");
+	}
+}
